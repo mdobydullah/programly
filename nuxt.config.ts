6b67f39b80a02b8735d89037bf63b86257f11a9b
Md Obydullah <hi@obydul.me>
2024-11-03 02:39:17 +0600

    add stack

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  extends: ['shadcn-docs-nuxt'],
-  compatibilityDate: '2024-07-06',
+    devtools: {enabled: true},
+    extends: ['shadcn-docs-nuxt'],
+    compatibilityDate: '2024-07-06',
+    mdc: {
+        highlight: {
+            langs: ['python']
+        }
+    }
 });
